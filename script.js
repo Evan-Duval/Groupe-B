a335bc9c78fa9064eb1cfd761b1724b2cddc399e
Evan-Duval <duvalevan368@gmail.com>
2024-10-21 16:44:20 +0200

    wip

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+
+
 // Ce code a été généré par une IA
-document.querySelectorAll('.hoverconsole').forEach(function(element) {
+let hoverconsole = document.querySelectorAll('.hoverconsole');
+hoverconsole.forEach(function(element) {
     element.addEventListener('mouseover', function() {
         console.log(element.textContent);
     });
 });
 // Fin du code généré par l'IA
-
-console.log("Test")
