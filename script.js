e4b1b42b0da3326b793dad974cf3ac46a7247066
Evan-Duval <duvalevan368@gmail.com>
2024-10-21 16:33:52 +0200

    add script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Ce code a été généré par une IA
+document.querySelectorAll('.hoverconsole').forEach(function(element) {
+    element.addEventListener('mouseover', function() {
+        console.log(element.textContent);
+    });
+});
+// Fin du code généré par l'IA
+
+console.log("Test")
